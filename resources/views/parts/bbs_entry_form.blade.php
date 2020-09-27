2cfd8bcfdb7ac9e1ac6736b5dc5ad4245d89b0ff
msy7822-ux <masa.1108@icloud.com>
2020-09-27 13:51:52 +0900

    display error message

--- resources/views/parts/bbs_entry_form.blade.php
+++ resources/views/parts/bbs_entry_form.blade.php
@@ -1,3 +1,15 @@
+<!-- エラーメッセージの表示 -->
+@if($errors->any())
+    <div class="errors">
+        <ul>
+            @foreach($errors->all() as $error) 
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<!-- フォーム -->
 <form action="{{ url('/create') }}" method="post">
     @csrf
 
